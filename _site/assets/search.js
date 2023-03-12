f827ec8e616bc9b8de6ea01901a1054d85fe2f12
mikucy <419714107@qq.com>
2023-03-12 22:21:41 +0800

    Add lab1 (#1)

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Hello World!",'url':"/2021/04/15/Hello-World.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Hello World!",'url':"/2021/04/15/Hello-World.html"},{'title':"THU OS Lab1",'url':"/2021/04/16/OSLab1.html"}]};
